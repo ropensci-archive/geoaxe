bfb299229a8cc0bf00fa5b7876e119a12d472b13
Scott Chamberlain <myrmecocystus@gmail.com>
2016-02-17 13:13:34 -0800

    added more tests, fix #3

--- R/to_wkt.R
+++ R/to_wkt.R
@@ -2,6 +2,13 @@
 #'
 #' @export
 #' @param x Input
+#' @examples
+#' library("rgeos")
+#' wkt <- "POLYGON((-180 -20, -140 55, 10 0, -140 -60, -180 -20))"
+#' poly <- readWKT(wkt)
+#' polys <- chop(x = poly)
+#' to_wkt(polys)
+#' to_wkt(polys)[[2]]
 to_wkt <- function(x) {
   UseMethod("to_wkt")
 }
